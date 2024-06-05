78de9e272c652a7f76a32ce68f7e906d89244930
EC2 Default User <ec2-user@ip-172-31-22-100.ec2.internal>
2024-06-05 14:23:32 +0000

    error with langchain

---
+++ qachatbot/qahomepage/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import langchain
+import langchain_community
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import HuggingFaceBgeEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.chains.question_answering import load_qa_chain
+# from langchain_community import HuggingFaceHub
+# from langchain.llms.huggingface_hub import HuggingFaceHub
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+
+
+
+import os
+from django.conf import settings
+import sentence_transformers
+
+
+# Create your views here.
+
+def home(request):
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+    module_dir = os.path.dirname(__file__)
+    file_path = os.path.join(module_dir, 'data.txt')
+    print(file_path)
+    loader = TextLoader(file_path)
+    document = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000 , chunk_overlap=0)
+    docs = text_splitter.split_documents(document)
+    embedding = HuggingFaceBgeEmbeddings()
+    db = FAISS.from_documents(docs , embedding)
+    query = "who is vikram?"
+    docs_similarity = db.similarity_search(query)
+    # llm = HuggingFaceHub(repo_id='google/flan-t5-xxl' , model_kwargs={"temprature":0.8 , "max_length":520})
+    tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-xxl")
+    llm = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-xxl")
+    chain = load_qa_chain(llm ,  chain_type="stuff")
+    response_text = chain.run(input_documents =docs_similarity , question = query )
+    return HttpResponse(response_text, content_type="text/plain")
